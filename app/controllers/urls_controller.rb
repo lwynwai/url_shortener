05bdca959e0eef28d88c199e8ef400690ea48252
lwynwai <wynwai11@gmail.com>
2017-02-17 17:18:33 +0800

    initialize create action

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,11 +1,30 @@
 class UrlsController < ApplicationController
+	
 	def index
 		@urls = Url.all
 	end
 
 	def show
-		byebug
-		@url = Url.find(params[:id])
+		@url = Url.find(params[:id])	
 	end
-	
+
+	def new
+		@url = Url.new
+	end
+
+	def create
+		@url = Url.new(url_params)
+		@url.shorten
+		if @url.save
+			redirect_to @url
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def url_params
+		params.require(:url).permit(:long_url)
+	end
+
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,11 +1,9 @@
 class Url < ApplicationRecord
-
-	 validates :original, :presence => true
-
+	
 	def shorten
   		range = [*'0'..'9', *'A'..'Z', *'a'..'z']
     		@short_url = (0...6).map{range.sample}.join
     		self.short_url=@short_url
   	end
-  	
+
 end
